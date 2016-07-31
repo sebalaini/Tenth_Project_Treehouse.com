7723138b8f509a9180d1070cd4951d94fec0b30d
Sebastiano <sebastiano@Sebas-MacBook-Pro.local>
2016-07-31 20:18:00 +0200

    sort image

--- js/main.js
+++ js/main.js
@@ -9,13 +9,26 @@ var flickrOptions = {
     var photoHTML = '<ul>';
     $.each(data.items,function(i,photo) {
       photoHTML += '<li>';
-      photoHTML += '<a href=" ' + photo.media.m + ' " data-lightbox="image" data-title=" Title: ' + photo.title + '. <br> Submitted: ' + photo.published + '. " ">';
+      photoHTML += '<a href=" ' + photo.media.m + ' " data-lightbox="image" data-title=" Title: ' + photo.title + '. <br> Submitted: ' + photo.published + '. <br> Tag: ' + photo.tags + '. " ">';
       photoHTML += '<img src="' + photo.media.m + ' " id="images"></a></li>';
     });
   photoHTML += '</ul>';
   $('.gallery').html(photoHTML);
   }
   $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+
+  $(".btn").click(function (event) {
+    event.preventDefault();//
+      var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+      var animal = $(this).text();
+      var flickrOptions = {
+        tags: animal,
+        format: "json"
+      };
+
+    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+  });
+
 });
 
 
